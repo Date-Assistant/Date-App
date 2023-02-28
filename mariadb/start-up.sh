8990c9d724fe09a27c7d4de08c9a2119115a4b1d
brian <bg339@njit.edu>
2023-02-28 07:47:37 +0000

    mariadb script update

--- mariadb/start-up.sh
+++ mariadb/start-up.sh
@@ -7,12 +7,12 @@ fi
 if [ "${RESPONSE}" = "true" ]; then
 	echo "MariaDB is running... Wait for status"
 	sleep 5
-	sudo systemctl status mariadb
+	systemctl status mariadb
 else
 	echo "MariaDB is not running. Enabling now"
 	sleep 5
-	sudo systemctl enable mariadb
-	sudo systemctl start mariadb
-	sudo systemctl status mariadb
+	systemctl enable mariadb
+	systemctl start mariadb
+	systemctl status mariadb
 
 fi
