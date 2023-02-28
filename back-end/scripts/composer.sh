2531466392b129e0ac62d836cb022401a17c6500
brian <bg339@njit.edu>
2023-02-28 09:08:53 +0000

    updated final composers

--- back-end/scripts/composer.sh
+++ back-end/scripts/composer.sh
@@ -2,12 +2,12 @@
 
 # database-server
 
-ssh -t it490admin@10.0.0.207 "sh Projects/Date-App/mariadb/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.207 "sh /home/it490admin/Projects/Date-App/mariadb/scripts/start-up.sh"
 
 # frontend-server
 
-ssh -t it490admin@10.0.0.156 "sh Projects/Date-App/front-end/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.156 "sh /home/it490admin/Projects/Date-App/front-end/scripts/start-up.sh"
 
 # rabbitmq-server
 
-ssh -t it490admin@10.0.0.218 "sh Projects/Date-App/rabbitmq/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.218 "sh /home/it490admin/Projects/Date-App/rabbitmq/scripts/start-up.sh"
--- front-end/scripts/composer.sh
+++ front-end/scripts/composer.sh
@@ -2,12 +2,12 @@
 
 # database-server
 
-ssh -t it490admin@10.0.0.207 "sh Projects/Date-App/mariadb/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.207 "sh /home/it490admin/Projects/Date-App/mariadb/scripts/start-up.sh"
 
 # backend-server
 
-ssh -t it490admin@10.0.0.108 "sh Projects/Date-App/back-end/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.108 "sh /home/it490admin/Projects/Date-App/back-end/scripts/start-up.sh"
 
 # rabbitmq-server
 
-ssh -t it490admin@10.0.0.218 "sh Projects/Date-App/rabbitmq/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.218 "sh /home/it490admin/Projects/Date-App/rabbitmq/scripts/start-up.sh"
