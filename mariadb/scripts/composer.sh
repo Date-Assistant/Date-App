f5162e0236c8853d375cda0329e332ce200fc823
brian <bg339@njit.edu>
2023-02-28 09:02:55 +0000

    final

--- mariadb/scripts/composer.sh
+++ mariadb/scripts/composer.sh
@@ -2,12 +2,12 @@
 
 # backend-server
 
-ssh -t it490admin@10.0.0.108 "sh /home/it490admin/Project/Date-App/back-end/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.108 "sh /home/it490admin/Projects/Date-App/back-end/scripts/start-up.sh"
 
 # frontend-server
 
-ssh -t it490admin@10.0.0.156 "sh Projects/Date-App/front-end/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.156 "sh /home/it490admin/Projects/Date-App/front-end/scripts/start-up.sh"
 
 # rabbitmq-server
 
-ssh -t it490admin@10.0.0.218 "sh Projects/Date-App/rabbitmq/scripts/start-up.sh"
+ssh -t it490admin@10.0.0.218 "sh /home/it490admin/Projects/Date-App/rabbitmq/scripts/start-up.sh"
