0762857dc3804a5dc067267c84b4f13532e43324
brian <bg339@njit.edu>
2023-02-28 07:14:00 +0000

    init db

---
+++ mariadb/initializedb.py
@@ -0,0 +1,23 @@
+# sudo apt update
+# sudo apt install mariadb-server
+# sudo systemctl start mariadb.service
+# sudo mysql_secure_installation
+# sudo mariadb
+# GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+
+
+import mysql.connector as mariadb
+mariadb_connection = mariadb.connect(host='localhost', user='admin', password='password', port='3306')
+cursor = mariadb_connection.cursor()
+cursor.execute("GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'admin'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'it490admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'it490admin'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'lap'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'lap'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'brian'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'brian'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("FLUSH PRIVILEGES;")
+cursor.execute("CREATE DATABASE IF NOT EXISTS IT490;")
+cursor.execute("CREATE DATABASE IF NOT EXISTS IT490BACKUP;")
+mariadb_connection.commit()
