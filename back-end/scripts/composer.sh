ca1b5500ad4c5af9fe4698656d7c98e383381e6c
brian <bg339@njit.edu>
2023-02-28 07:02:09 +0000

    new db script

--- back-end/scripts/composer.sh
+++ back-end/scripts/composer.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-sudo systemctl enable apache2
-sudo systemctl start apache2
-sudo systemctl status apache2
+
+# database-server
+
+ssh -t it490admin@10.0.0.207 "sudo systemctl is-active --quiet ssh" 
+
+
+#sudo systemctl enable apache2
+#sudo systemctl start apache2
+#sudo systemctl status apache2
