aeb461ce7f0414d4f790c49bbd17aa4cd685ca54
bg339 <bg339@njit.edu>
2023-03-11 05:09:32 -0500

    front end: new landing page new register form

--- front-end/flask_app/myapp.py
+++ front-end/flask_app/myapp.py
@@ -2,27 +2,26 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 import pika
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'd523a32875c96254372519418f55af57b892afc7b7e4401f'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
             {'title': 'Message Two',
              'content': 'Message Two Content'}
             ]
-
+'''
 credentials = pika.PlainCredentials('thebigrabbit','it490')
-parameters = pika.ConnectionParameters('10.241.51.24',5672,'cherry_broker',credentials)
+parameters = pika.ConnectionParameters('10.0.0.218',5672,'cherry_broker',credentials)
 
 connection = pika.BlockingConnection(parameters)
 
 channel = connection.channel()
 
 channel.queue_declare(queue='This is register send test')
-
+'''
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
 
 # ...
 
@@ -38,15 +37,47 @@ def create():
                 flash('Content is required!')
             else:
                 messages.append({'title': title, 'content': content})
+                '''
                 channel.basic_publish(exchange='',
                 routing_key='This is register send test',
                 body = '%s : %s' % (title,content))
                 connection.close()
+                '''
                 return redirect(url_for('index'))
 
         return render_template('create.html')
 
+@app.route('/register/', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        # Retrieve the form data
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        phone = request.form['phone']
+        address = request.form['address']
+        zip = request.form['zip']
+        email_toggle = request.form.get('email-toggle', 'off')
+
+        # Create a dictionary to store the form data
+        user_data = {
+            'first_name': fname,
+            'last_name': lname,
+            'email': email,
+            'phone': phone,
+            'address': address,
+            'zip_code': zip,
+            'receive_emails': email_toggle
+        }
+
+        # Print the form data
+        print(user_data)
+
+        # TODO: Add code to store the data in a database or message queue
+
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=7007)
+	app.run(host='localhost', port=7007)
 
