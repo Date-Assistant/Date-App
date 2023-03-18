6d966b1acde947511c3483905119b91cefd98c20
Lap Le <98070780+ll452@users.noreply.github.com>
2023-03-17 22:37:04 -0400

    Update Send.py

---
+++ plugins/Send.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import pika
+
+
+class send:
+	def __init__(self,ip_addr,port,username,password,vhost,exchange,queue,routing_key,exchange_type):
+  			self.ip_addr = ip_addr
+        self.port = port
+        self.username = username
+        self.password = password
+        self.vhost = vhost
+        self.exchange = exchange
+        self.exchange_type = exchange_type
+        self.routing_key = routing_key
+        self.queue = queue
+  	    self.credentials = pika.PlainCredentials(self.username, self.password)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.ip_addr, self.port, self.vhost, self.credentials))
+        self.channel = self.connection.channel()
+        
+  def send_message(self, message):
+  			# self.channel.exchange_declare(exchange='', exchange_type='fanout')
+        self.message = message
+        self.channel.queue_declare(queue=self.queue)
+        self.channel.basic_publish(self.exchange, self.routing_key, body=self.message)
+        print(f"Sent message: {self.message}")
