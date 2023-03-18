cda15e08678ee50b3118217576865c1d102c56a0
bg339@njit.edu <bg339@njit.edu>
2023-03-18 03:15:41 +0000

    changes

--- back-end/rabbitmq-scripts/receive_registration.py
+++ back-end/rabbitmq-scripts/receive_registration.py
@@ -1,4 +1,3 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
 import pika
 import sys
 import json
@@ -17,8 +16,17 @@ exchange_type = 'direct'
 routing_key = 'hello'
 
 def main():
-  try:
+	try:
 		backend_receive = receive(ip_addr,port,username,password,vhost,queue,routing_key,exchange_type)
 		backend_receive.receive_from_frontend(queue)
 	except BaseException:
 		print("error")
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except:
+            sys.exit(0)
