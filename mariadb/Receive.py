883666cdfc37da5acef16576cd382d6757492d39
Lap Le <98070780+ll452@users.noreply.github.com>
2023-03-19 03:44:16 -0400

    Update Receive.py

--- mariadb/Receive.py
+++ mariadb/Receive.py
@@ -3,7 +3,7 @@ import pika
 import json
 
 class recieve:
-      def __init__(self,ip_addr,port,username,password,vhost,exchange,queue,routing_key,exchange_type):
+      def __init__(self,ip_addr,port,username,password,vhost,exchange,queue,routing_key):
             self.ip_addr = ip_addr
             self.port = port
             self.username = username
