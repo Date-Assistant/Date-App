8359562e1d5a5d282b28c040cb977e0f8bf29aaf
bg339@njit.edu <bg339@njit.edu>
2023-03-19 09:10:32 +0000

    updates

--- back-end/rabbitmq-scripts/receive_registration.py
+++ back-end/rabbitmq-scripts/receive_registration.py
@@ -16,10 +16,10 @@ fe_routing_key = 'fe2be'
 db_routing_key = 'be2db'
 
 def main():
-    registration_receive = Receive.recieve(ip_addr,port,username,password,vhost,fe_exchange,fe_declare_queue,fe_routing_key)
+    registration_receive = Receive.recieve(ip_addr,port,username,password,vhost,'fe2be',fe_declare_queue,fe_routing_key)
     frontend_data = {}
     result = registration_receive.receive_from_frontend(frontend_data)
-
+    print(result)
     
     back_end_to_db = Send.send(ip_addr,port,username,password,vhost,db_exchange,db_routing_key)
     data_to_db = json.dumps(result)
