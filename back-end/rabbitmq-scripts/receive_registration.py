9802caf222764169d85f3b143054f9c244cf019e
Brian <bg339@njit.edu>
2023-03-19 07:55:35 -0400

    testing some updates

--- back-end/rabbitmq-scripts/receive_registration.py
+++ back-end/rabbitmq-scripts/receive_registration.py
@@ -22,7 +22,7 @@ db_routing_key = 'dbqueue'
 def main():
     backend_receive = Receive.recieve(ip_addr,port,username,password,vhost,front_end_queue,front_end_routing_key,front_end_exchange_type)
     frontend_data = {}
-    result = backend_receive.receive_from_frontend(front_end_queue,frontend_data)
+    result = backend_receive.receive_from_frontend(exchange,front_end_queue,frontend_data)
     back_end_to_db = Send.send(ip_addr,port,username,password,vhost,exchange,db_queue,db_routing_key,db_exchange_type)
     data_to_db = json.dumps(result)
     back_end_to_db.send_message(data_to_db)
