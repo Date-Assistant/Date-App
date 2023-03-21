d975b252fac4db033520f5a6d232403e9f7913bf
bg339 <98499966+bg339@users.noreply.github.com>
2023-03-21 05:41:45 -0400

    updates

--- back-end/rabbitmq-scripts/receive_signin.py
+++ back-end/rabbitmq-scripts/receive_signin.py
@@ -83,7 +83,7 @@ def main():
         back_end_to_fe = Send.send(ip_addr,port,username,password,vhost,send_to_exchange,fe_userexist_queue,fe_userexist_routing_key,db_exchange_type)
         data_to_fe = json.dumps(sendUserDetails)
         back_end_to_fe.send_message(data_to_fe)
-    elif(result1['reply'] == "False"):
+    elif(result1['reply'] == "false"):
         sendUserDetails1 = {'error':'user does not exist'}
         back_end_to_fe1 = Send.send(ip_addr,port,username,password,vhost,send_to_exchange,fe_usernoexist_queue,fe_usernoexist_routing_key,db_exchange_type)
         data_to_fe1 = json.dumps(sendUserDetails1)
