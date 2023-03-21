6ca38d4301721895d058cee98b541e417df90280
bg339 <98499966+bg339@users.noreply.github.com>
2023-03-21 02:51:31 -0400

    updates

--- front-end/flaskapp/myapp.py
+++ front-end/flaskapp/myapp.py
@@ -67,7 +67,7 @@ def signin():
             json_user_data = json.dumps(user_sign_in)
             front_end_sign_in.send_message(json_user_data)
             
-            receive_sign_in = Receive.recieve(ip_addr, port, username, password, vhost, exchange, signin_queue, signin_routing_key, exchange_type)
+            receive_sign_in = Receive.recieve(ip_addr, port, username, password, vhost, signin_queue, signin_routing_key,  exchange, exchange_type)
             json_response = {}
             receive_sign_in.receive_message(json_response)
 
