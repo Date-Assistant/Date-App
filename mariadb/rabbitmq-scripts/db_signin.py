039d5caeeaf3dcc3d2a852d04e1faf527904e8a9
bg339 <98499966+bg339@users.noreply.github.com>
2023-03-28 17:17:40 -0400

    updates

--- mariadb/rabbitmq-scripts/db_signin.py
+++ mariadb/rabbitmq-scripts/db_signin.py
@@ -30,7 +30,7 @@ mariadb_connection = mariadb.connect(host='localhost', user='root', password='pa
 cursor = mariadb_connection.cursor()
 
 def main():
-    db_receive = Receive.recieve(ip_addr,port,username,password,vhost,receiving_exchange,db_queue,db_routing_key,receiving_exchange,db_exchange_type)
+    db_receive = Receive.recieve(ip_addr,port,username,password,vhost,db_queue,db_routing_key,receiving_exchange,db_exchange_type)
     backend_data = {}
     while True:
         result = db_receive.receive_message(backend_data)
