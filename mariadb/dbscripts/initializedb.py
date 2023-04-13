1181d6d7921947a340dad9b9d700847692e26023
root <root@ip-172-31-86-28.ec2.internal>
2023-04-13 03:54:46 +0000

    updates to mariadb cluster and backend

--- mariadb/dbscripts/initializedb.py
+++ mariadb/dbscripts/initializedb.py
@@ -6,19 +6,20 @@
 # sudo mariadb
 # GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
 
-
 import mysql.connector as mariadb
-mariadb_connection = mariadb.connect(host='localhost', user='root', password='password', port='3306')
+mariadb_connection = mariadb.connect(host='0.0.0.0', user='root', password='password', port='3306')
 cursor = mariadb_connection.cursor()
+cursor.execute("GRANT ALL ON *.* TO 'admin'@'0.0.0.0' IDENTIFIED BY 'password' WITH GRANT OPTION;")
 cursor.execute("GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'admin'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'it490admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'it490admin'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'lap'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'lap'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'brian'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;")
-cursor.execute("GRANT ALL ON *.* TO 'brian'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+#cursor.execute("GRANT ALL ON *.* TO 'admin'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'it490admin'@'0.0.0.0' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+#cursor.execute("GRANT ALL ON *.* TO 'it490admin'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'lap'@'0.0.0.0' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+#cursor.execute("GRANT ALL ON *.* TO 'lap'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
+cursor.execute("GRANT ALL ON *.* TO 'brian'@'0.0.0.0' IDENTIFIED BY 'password' WITH GRANT OPTION;")
+#cursor.execute("GRANT ALL ON *.* TO 'brian'@10.0.0.207 IDENTIFIED BY 'password' WITH GRANT OPTION;")
 cursor.execute("FLUSH PRIVILEGES;")
 cursor.execute("CREATE DATABASE IF NOT EXISTS IT490;")
 cursor.execute("CREATE DATABASE IF NOT EXISTS IT490BACKUP;")
 mariadb_connection.commit()
+mariadb_connection.close()
