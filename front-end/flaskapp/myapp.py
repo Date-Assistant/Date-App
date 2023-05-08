5f7abdb4ea8b0a7137a5f1f5456712bae53e0ad7
ll452 <ll452@njit.edu>
2023-05-08 17:12:25 -0400

    adding api stuff

--- front-end/flaskapp/myapp.py
+++ front-end/flaskapp/myapp.py
@@ -9,17 +9,44 @@ import tempfile
 import time
 import requests
 import threading
+from yelpapi import YelpAPI
 
 
 app = Flask(__name__)
 #yelp_api_key 
 #api_key = open('API_KEY.txt').read() 
 api_key = 'C6oVTJvz932BtQjLQroxFp_dgk4gRkVJMD0Tthr0ThYI7W1RDuFR5p2I2ipKnBWvkvjF4LEehQZ-Fh5DcdRDCJvEPlx8A6h4OZY8eAO4Q5DvlYcl2GkT93ZYGXsTZHYx'
+YELP_API_KEY = "C6oVTJvz932BtQjLQroxFp_dgk4gRkVJMD0Tthr0ThYI7W1RDuFR5p2I2ipKnBWvkvjF4LEehQZ-Fh5DcdRDCJvEPlx8A6h4OZY8eAO4Q5DvlYcl2GkT93ZYGXsTZHYx"
+WEATHER_API_KEY = "5d4ff4f2e99e0cce15a54a4f247fcc58"
+yelp_api = YelpAPI(YELP_API_KEY)
 
+"""
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = tempfile.gettempdir()
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'fallback secret key')  # Use fallback secret key if not found in environment variables
 Session(app)
+"""
+#helper functions
+def get_weather_data(location, api_key):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    return response.json()
+
+def get_yelp_data(location, term, preferences):
+    activity_mapping = {
+        'food': 'restaurants',
+        'outdoors': 'outdoor, hiking, park, trails',
+        'arts': 'arts, museums, galleries',
+        'entertainment': 'entertainment, movies, theater'
+    }
+    activity_term = activity_mapping.get(term, term)
+    if term == 'food' and preferences != 'any':
+        activity_term = f"{activity_term}, {preferences}"
+
+    response = yelp_api.search_query(term=activity_term, location=location, sort_by='best_match', limit=10)
+    return response.get('businesses', [])
 
 @app.route('/')
 def index():
@@ -302,6 +329,10 @@ def register2():
 def search_restaurants():
     return render_template('search_restaurants.html')
 
+@app.route('/activity_recommender')
+def activity_recommender():
+    return render_template('activity_recommender.html')
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     API_ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
@@ -346,6 +377,42 @@ def business_details(id):
 
     # Render the HTML template with the business details
     return render_template('details.html', business=business)
+
+@app.route('/recommendation', methods=['POST'])
+def recommendation():
+    weather = request.form.get('weather')
+    time_of_day = request.form.get('time_of_day')
+    activity = request.form.get('activity')
+    preferences = request.form.get('preferences')
+    budget = request.form.get('budget')
+    location = request.form.get('location')
+
+    weather_data = get_weather_data(location, WEATHER_API_KEY)
+    if not weather_data:
+        error_message = f"Invalid location: {location}. Please try again with a valid location."
+        return render_template('error.html', error_message=error_message)
+
+    yelp_data = get_yelp_data(location, activity, preferences)
+    if not yelp_data:
+        error_message = f"No recommendations found for the given location and activity. Please try again with different inputs."
+        return render_template('error.html', error_message=error_message)
+
+    # Use yelp_data as a list of recommendations
+    recommendations = yelp_data
+
+    # Check if current weather matches the user's preferred weather
+    current_weather = weather_data.get("weather", [{}])[0].get("main", "").lower()
+    weather_warning = None
+    if weather == "sunny" and "clear" not in current_weather:
+        weather_warning = "The current weather does not match your preferred sunny weather."
+    elif weather == "rainy" and "rain" not in current_weather:
+        weather_warning = "The current weather does not match your preferred rainy weather."
+    elif weather == "cloudy" and "clouds" not in current_weather:
+        weather_warning = "The current weather does not match your preferred cloudy weather."
+
+    return render_template('recommendation.html', recommendations=recommendations, warning=weather_warning)
+
+
 
 
 if __name__ == '__main__':
